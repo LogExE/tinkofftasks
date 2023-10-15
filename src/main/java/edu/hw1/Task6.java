926a57f5f8150807feb0e0d73d8d03361e460525
LogXx <awesome149712@gmail.com>
2023-10-15 14:18:44 +0400

    task6: move 4 into constant

--- src/main/java/edu/hw1/Task6.java
+++ src/main/java/edu/hw1/Task6.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 
 public class Task6 {
     private static final int KAPREKAR_NUMBER = 6174;
+    private static final int DIGITS_COUNT = 4;
 
     private Task6() {
 
@@ -16,21 +17,21 @@ public class Task6 {
         if (x <= 1000 || x >= 10000) {
             throw new IllegalArgumentException("Number must consist of 4 digits and be greater than 1000!");
         }
-        int[] arr = new int[4];
-        for (int i = 0; i < 4; ++i) {
+        int[] arr = new int[DIGITS_COUNT];
+        for (int i = 0; i < DIGITS_COUNT; ++i) {
             arr[i] = x % 10;
             x /= 10;
         }
         Arrays.sort(arr);
-        if (arr[0] == arr[3]) {
+        if (arr[0] == arr[DIGITS_COUNT - 1]) {
             throw new IllegalArgumentException("Number must contain at least two distinct digits!");
         }
         int descending = 0;
-        for (int i = 0; i < 4; ++i) {
+        for (int i = 0; i < DIGITS_COUNT; ++i) {
             descending = descending * 10 + arr[i];
         }
         int ascending = 0;
-        for (int i = 3; i >= 0; --i) {
+        for (int i = DIGITS_COUNT - 1; i >= 0; --i) {
             ascending = ascending * 10 + arr[i];
         }
         return 1 + countK(ascending - descending);
