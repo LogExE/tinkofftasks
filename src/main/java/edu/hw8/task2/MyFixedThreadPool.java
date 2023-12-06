83d0bd4eda14c45856d658bc5321f603d4c30399
LogExE <awesome149712@gmail.com>
2023-12-06 12:59:56 +0400

    rework

--- src/main/java/edu/hw8/task2/MyFixedThreadPool.java
+++ src/main/java/edu/hw8/task2/MyFixedThreadPool.java
@@ -2,34 +2,25 @@ package edu.hw8.task2;
 
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class MyFixedThreadPool implements ThreadPool {
-    private Thread[] threads;
+    private static final Logger LOGGER = LogManager.getLogger();
+    private final Thread[] threads;
     private final BlockingQueue<Runnable> tasks = new LinkedBlockingQueue<>();
 
     public static ThreadPool create(int n) {
         if (n <= 0) {
             throw new IllegalArgumentException("Threads count must be positive!");
         }
-        MyFixedThreadPool pool = new MyFixedThreadPool();
-        pool.threads = new Thread[n];
-        return pool;
+        return new MyFixedThreadPool(n);
     }
 
     @Override
     public void start() {
-        for (int i = 0; i < threads.length; ++i) {
-            threads[i] = new Thread(() -> {
-                while (true) {
-                    try {
-                        tasks.take().run();
-                    } catch (InterruptedException e) {
-                        Thread.currentThread().interrupt();
-                        break;
-                    }
-                }
-            });
-            threads[i].start();
+        for (Thread thread : threads) {
+            thread.start();
         }
     }
 
@@ -49,6 +40,25 @@ public class MyFixedThreadPool implements ThreadPool {
     public void close() {
         for (Thread thread : threads) {
             thread.interrupt();
+        }
+    }
+
+    private MyFixedThreadPool(int n) {
+        threads = new Thread[n];
+        for (int i = 0; i < threads.length; ++i) {
+            Runnable tasksDo = () -> {
+                while (true) {
+                    try {
+                        tasks.take().run();
+                    } catch (InterruptedException e) {
+                        Thread.currentThread().interrupt();
+                        break;
+                    } catch (RuntimeException e) {
+                        LOGGER.error(e.getMessage(), e);
+                    }
+                }
+            };
+            threads[i] = new Thread(tasksDo);
         }
     }
 }
