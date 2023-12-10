3e6b06d905153309924f2f77e03798a73e87e497
LogXx <awesome149712@gmail.com>
2023-12-10 23:30:38 +0400

    oopsie

--- src/main/java/edu/hw9/task1/StatsCollector.java
+++ src/main/java/edu/hw9/task1/StatsCollector.java
@@ -2,18 +2,22 @@ package edu.hw9.task1;
 
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class StatsCollector {
-    ConcurrentHashMap<String, ProducedStats> metrics;
+    private final ConcurrentHashMap<String, ProducedStats> metrics;
+    private final AtomicInteger working;
 
     public StatsCollector() {
         metrics = new ConcurrentHashMap<>();
+        working = new AtomicInteger(0);
     }
 
     public void push(String metricName, double[] vals) {
         if (vals.length == 0) {
             throw new IllegalArgumentException("Array should not be empty!");
         }
+        working.incrementAndGet();
         new Thread(() -> {
             double max = -Double.MAX_VALUE;
             double min = Double.MAX_VALUE;
@@ -25,10 +29,14 @@ public class StatsCollector {
             }
             double avg = sum / vals.length;
             metrics.put(metricName, new ProducedStats(min, max, avg, sum));
+            working.decrementAndGet();
         }).start();
     }
 
     public Map<String, ProducedStats> stats() {
+        while (working.get() != 0) {
+
+        }
         return Map.copyOf(metrics);
     }
 }
